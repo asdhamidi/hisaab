e3bbd0cf4adfc5081237a66f12a9c2b319902bc5
Asadullah Hamidi <asad.hamidi119@gmail.com>
2024-11-23 20:10:17 +0530

    Login page makeover

--- src/components/login.js
+++ src/components/login.js
@@ -75,6 +75,31 @@ const Login = ({ loadEntries, setloggedIn }) => {
       />
       {loggigIn === false && (
         <>
+          <div className="headings">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.25"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="icon icon-tabler icons-tabler-outline icon-tabler-calculator"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 3m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+              <path d="M8 7m0 1a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-6a1 1 0 0 1 -1 -1z" />
+              <path d="M8 14l0 .01" />
+              <path d="M12 14l0 .01" />
+              <path d="M16 14l0 .01" />
+              <path d="M8 17l0 .01" />
+              <path d="M12 17l0 .01" />
+              <path d="M16 17l0 .01" />
+            </svg>
+            <h1>हिसाब</h1>
+          </div>
           <div className="front-page-contents">
             <div className="front-page-options">
               <button
@@ -85,7 +110,27 @@ const Login = ({ loadEntries, setloggedIn }) => {
                   setregister(false);
                 }}
               >
-                login
+                {register ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="icon icon-tabler icons-tabler-outline icon-tabler-login-2"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M9 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" />
+                    <path d="M3 12h13l-3 -3" />
+                    <path d="M13 15l3 -3" />
+                  </svg>
+                ) : (
+                  "login"
+                )}
               </button>
               <button
                 className={registerClass}
@@ -95,7 +140,28 @@ const Login = ({ loadEntries, setloggedIn }) => {
                   setregister(true);
                 }}
               >
-                register
+                {register ? (
+                  "register"
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="icon icon-tabler icons-tabler-outline icon-tabler-door-enter"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M13 12v.01" />
+                    <path d="M3 21h18" />
+                    <path d="M5 21v-16a2 2 0 0 1 2 -2h6m4 10.5v7.5" />
+                    <path d="M21 7h-7m3 -3l-3 3l3 3" />
+                  </svg>
+                )}
               </button>
             </div>
             {register === false && (
@@ -159,6 +225,7 @@ const Login = ({ loadEntries, setloggedIn }) => {
                 </button>
               </form>
             )}
+            <div className="credits">made with ❤️</div>
           </div>
         </>
       )}
